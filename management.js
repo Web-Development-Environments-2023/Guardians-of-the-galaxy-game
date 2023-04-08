62fe38dfc0658c17ab770dd13a06e6d73727b1ac
LiorMish <liormishutin24@gmail.com>
2023-04-08 15:55:39 +0300

    balagan 2

--- management.js
+++ management.js
@@ -1,43 +1,51 @@
-var canvas;
+const managementDiv = document.getElementById("managementDiv");
+
 const loginButton = document.getElementById("loginButton");
 const loginDiv = document.getElementById("loginDiv");
 
 const registerButton = document.getElementById("registerButton");
-const registerDiv = document.getElementById("loginDiv");
+const registerDiv = document.getElementById("registerDiv");
 
-// start a new game when user clicks Start Game button
-loginButton.addEventListener("click", function() {
-  loginDiv.style.display = "block";
-});
-
-window.addEventListener("load", setupGame, false);
-
-// called when the app first launches
-function setupGame()
-{
-  // Get the canvas
-  canvas = document.getElementById("gameCanvas");
-  ctx = canvas.getContext("2d");
-
-  //sound
-  //obj = document.getElementById( "targetSound" );
-  // Background image
-  //bgImage = new Image();
-  //bgImage.src = "images/background.png";
-  // Hero image
-  //heroImage = new Image();
-  //heroImage.src = "images/hero.png";
-  // Monster image
-  //monsterImage = new Image();
-  //monsterImage.src = "images/monster.png";
-  // Game objects
-
-
-  //hero = {speed: 256 }; // movement in pixels per second
-
-  // Handle keyboard controls
-  keysDown = {};
+function giveFocusToDiv(divToFocus) {
+  appDivs = ["loginDiv", "registerDiv", "managementDiv"] //ADD MORE DIVS WHEN THEY ARE ADDED TO THE APP!!
+  for(let i=0; i<appDivs.length; i++) {
+      document.getElementById(appDivs[i]).style.display = "none"
+  }
+  divToFocus.style.display = "block"
 }
+
+loginButton.addEventListener("click", function() {giveFocusToDiv(loginDiv)}, false);
+
+registerButton.addEventListener("click", function() {giveFocusToDiv(registerDiv)}, false);
+
+// window.addEventListener("load", setupGame, false);
+
+// // called when the app first launches
+// function setupGame()
+// {
+//   // Get the canvas
+//   canvas = document.getElementById("gameCanvas");
+//   ctx = canvas.getContext("2d");
+
+//   //sound
+//   //obj = document.getElementById( "targetSound" );
+//   // Background image
+//   //bgImage = new Image();
+//   //bgImage.src = "images/background.png";
+//   // Hero image
+//   //heroImage = new Image();
+//   //heroImage.src = "images/hero.png";
+//   // Monster image
+//   //monsterImage = new Image();
+//   //monsterImage.src = "images/monster.png";
+//   // Game objects
+
+
+//   //hero = {speed: 256 }; // movement in pixels per second
+
+//   // Handle keyboard controls
+//   keysDown = {};
+// }
 
   // Check for keys pressed where key represents the keycode
 
