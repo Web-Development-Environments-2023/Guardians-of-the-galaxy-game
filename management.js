48a4037f68f11106cad77c4e8f7a91daa87b6a3f
LiorMish <liormishutin24@gmail.com>
2023-04-08 20:38:32 +0300

    config

--- management.js
+++ management.js
@@ -2,13 +2,14 @@
 const mainDiv = document.getElementById("mainDiv");
 const loginButton = document.getElementById("loginButton");
 const registerButton = document.getElementById("registerButton");
+const configDiv = document.getElementById("configDiv");
 
 //Login page attributes
 const loginDiv = document.getElementById("loginDiv");
 const loginBackButton = document.getElementById("backFromLoginButton");
 var loginUsername = document.getElementById("loginUsername");
 var loginPassword = document.getElementById("loginPassword");
-var loginSubmit = document.getElementById("loginSubmit");
+const loginSubmit = document.getElementById("loginSubmit");
 
 //Register page attributes
 const registerDiv = document.getElementById("registerDiv");
@@ -23,7 +24,7 @@ const users = {
 };
 
 function giveFocusToDiv(divToFocus) {
-  appDivs = ["loginDiv", "registerDiv", "mainDiv"] //ADD MORE DIVS WHEN THEY ARE ADDED TO THE APP!!
+  appDivs = ["loginDiv", "registerDiv", "mainDiv", "configDiv"] //ADD MORE DIVS WHEN THEY ARE ADDED TO THE APP!!
   for(let i=0; i<appDivs.length; i++) {
       document.getElementById(appDivs[i]).style.display = "none"
   }
@@ -36,16 +37,21 @@ registerButton.addEventListener("click", function() {giveFocusToDiv(registerDiv)
 
 //Login page methods
 loginBackButton.addEventListener("click", function() {giveFocusToDiv(mainDiv)}, false);
-// loginSubmit.addEventListener("click", function() {loginUser(loginUsername.value, loginPassword.value)})
-loginSubmit.addEventListener("click", function() {console.log("hi hi")});
+loginSubmit.addEventListener("click", function() {loginUser(loginUsername.value, loginPassword.value)})
+// loginSubmit.addEventListener("click", function() {console.log("hi hi")});
 
 
 function loginUser(username, password) {
   if (users[username] && users[username].password === password) {
     console.log("Login successful.");
+    giveFocusToDiv(configDiv);
   } else {
     console.log("Invalid username or password.");
+    alert("Invalid username or password. Please try again.");
+    loginUsername.value = "";
+    loginPassword.value = "";
   }
+  sleep(2000);
 }
 
 //Register page methods
@@ -61,4 +67,12 @@ function registerUser(username, password) {
     };
     console.log("User registered successfully.");
   }
+}
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
 }
