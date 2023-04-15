36eff0fea78aefddbec763b4cf5152e87cb11353
LiorMish <liormishutin24@gmail.com>
2023-04-15 11:00:44 +0300

    register

--- management.js
+++ management.js
@@ -14,13 +14,14 @@ const loginSubmit = document.getElementById("loginSubmit");
 //Register page attributes
 const registerDiv = document.getElementById("registerDiv");
 const registerBackButton = document.getElementById("backFromRegisterButton");
+var registerUsername = document.getElementById("registerUsername");
+var registerPassword = document.getElementById("registerPassword");
+var registerRepeatPassword = document.getElementById("repeat-password");
+const registerSubmit = document.getElementById("registerSubmit");
+
 
 //Users dictionary
-const users = {
-  s: {
-    password: "s"
-  }
-};
+var users = {"s" : "s"};
 
 function giveFocusToDiv(divToFocus) {
   appDivs = ["loginDiv", "registerDiv", "mainDiv", "configDiv"] //ADD MORE DIVS WHEN THEY ARE ADDED TO THE APP!!
@@ -37,11 +38,9 @@ registerButton.addEventListener("click", function() {giveFocusToDiv(registerDiv)
 //Login page methods
 loginBackButton.addEventListener("click", function() {giveFocusToDiv(mainDiv)}, false);
 loginSubmit.addEventListener("click", function() {loginUser(loginUsername.value, loginPassword.value)})
-// loginSubmit.addEventListener("click", function() {console.log("hi hi")});
-
 
 function loginUser(username, password) {
-  if (users[username] && users[username].password === password) {
+  if (users[username] && users[username] === password) {
     console.log("Login successful.");
     giveFocusToDiv(configDiv);
   } else {
@@ -54,16 +53,29 @@ function loginUser(username, password) {
 
 //Register page methods
 registerBackButton.addEventListener("click", function() {giveFocusToDiv(mainDiv)}, false);
+registerSubmit.addEventListener("click", function() {registerUser(registerUsername.value, registerPassword.value, registerRepeatPassword.value)})
 
-function registerUser(username, password) {
+function registerUser(username, password, repeat) {
   if (users[username]) {
     console.log("User already exists.");
-  } else {
-    users[username] = {
-      username: username,
-      password: password
-    };
+    alert("User already exists. Please try again.");
+    registerUsername.value = ""
+    registerPassword.value = ""
+    registerRepeatPassword.value = ""
+  } else if (password != repeat){
+    console.log("Password is incorrect.")
+    alert("Password is incorrect. Please try again.");
+    registerUsername.value = ""
+    registerPassword.value = ""
+    registerRepeatPassword.value = ""
+  }
+  else{
+    users[username] = password
     console.log("User registered successfully.");
+    for (const key of Object.keys(users)) {
+      console.log(key + ":" + users[key])
+    }
+    giveFocusToDiv(mainDiv)
   }
 }
 
