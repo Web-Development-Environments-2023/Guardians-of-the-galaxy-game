dd2c85b6787c94126c3312211118754074f951de
LiorMish <liormishutin24@gmail.com>
2023-04-17 20:44:33 +0300

    welcome page

--- management.js
+++ management.js
@@ -1,42 +1,42 @@
 //Main page attributes
-const mainDiv = document.getElementById("mainDiv");
+const logoDiv = document.getElementById("game_logo");
+const homeButton = document.getElementById("homeButton");
 const loginButton = document.getElementById("loginButton");
 const registerButton = document.getElementById("registerButton");
 const configDiv = document.getElementById("configDiv");
 
 //Login page attributes
 const loginDiv = document.getElementById("loginDiv");
-const loginBackButton = document.getElementById("backFromLoginButton");
 var loginUsername = document.getElementById("loginUsername");
 var loginPassword = document.getElementById("loginPassword");
 const loginSubmit = document.getElementById("loginSubmit");
 
 //Register page attributes
 const registerDiv = document.getElementById("registerDiv");
-const registerBackButton = document.getElementById("backFromRegisterButton");
 var registerUsername = document.getElementById("registerUsername");
 var registerPassword = document.getElementById("registerPassword");
 var registerRepeatPassword = document.getElementById("repeat-password");
 const registerSubmit = document.getElementById("registerSubmit");
 
+//Divs
+var appDivs = [logoDiv, loginDiv, registerDiv, configDiv] //ADD MORE DIVS WHEN THEY ARE ADDED TO THE APP!!
 
 //Users dictionary
 var users = {"s" : "s"};
 
 function giveFocusToDiv(divToFocus) {
-  appDivs = ["loginDiv", "registerDiv", "mainDiv", "configDiv"] //ADD MORE DIVS WHEN THEY ARE ADDED TO THE APP!!
   for(let i=0; i<appDivs.length; i++) {
-      document.getElementById(appDivs[i]).style.display = "none"
+      appDivs[i].style.display = "none"
   }
   divToFocus.style.display="block"
 }
 
 //Main page methods
+homeButton.addEventListener("click", function() {giveFocusToDiv(logoDiv)}, false);
 loginButton.addEventListener("click", function() {giveFocusToDiv(loginDiv)}, false);
 registerButton.addEventListener("click", function() {giveFocusToDiv(registerDiv)}, false);
 
 //Login page methods
-loginBackButton.addEventListener("click", function() {giveFocusToDiv(mainDiv)}, false);
 loginSubmit.addEventListener("click", function() {loginUser(loginUsername.value, loginPassword.value)})
 
 function loginUser(username, password) {
@@ -52,10 +52,10 @@ function loginUser(username, password) {
 }
 
 //Register page methods
-registerBackButton.addEventListener("click", function() {giveFocusToDiv(mainDiv)}, false);
 registerSubmit.addEventListener("click", function() {registerUser(registerUsername.value, registerPassword.value, registerRepeatPassword.value)})
 
 function registerUser(username, password, repeat) {
+  console.log("hi hi")
   if (users[username]) {
     console.log("User already exists.");
     alert("User already exists. Please try again.");
@@ -65,7 +65,6 @@ function registerUser(username, password, repeat) {
   } else if (password != repeat){
     console.log("Password is incorrect.")
     alert("Password is incorrect. Please try again.");
-    registerUsername.value = ""
     registerPassword.value = ""
     registerRepeatPassword.value = ""
   }
@@ -75,7 +74,7 @@ function registerUser(username, password, repeat) {
     for (const key of Object.keys(users)) {
       console.log(key + ":" + users[key])
     }
-    giveFocusToDiv(mainDiv)
+    giveFocusToDiv(loginDiv)
   }
 }
 
