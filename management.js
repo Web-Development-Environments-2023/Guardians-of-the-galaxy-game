7a7b12f73b4d5694f31f6159450dbef5c0a0aedf
LiorMish <liormishutin24@gmail.com>
2023-04-20 23:04:58 +0300

    pop ups complete

--- management.js
+++ management.js
@@ -1,4 +1,4 @@
-//Main page attributes
+//Main page 
 const logoDiv = document.getElementById("game_logo");
 const homeButton = document.getElementById("homeButton");
 const loginButton = document.getElementById("loginButton");
@@ -8,7 +8,14 @@ const welcome_login_button = document.getElementById("welcome_login_button");
 const welcome_register_button = document.getElementById("welcome_register_button");
 const footer = document.getElementById("footer");
 
-//Login page attributes
+homeButton.addEventListener("click", function() {if (isGameOn()){giveFocusToDiv(end_game_pop_up)}else{giveFocusToDiv(logoDiv)}}, false);
+loginButton.addEventListener("click", function() {if (isGameOn()){giveFocusToDiv(end_game_pop_up)}else{changeLoginText(); giveFocusToDiv(loginDiv)}}, false);
+welcome_login_button.addEventListener("click", function() {giveFocusToDiv(loginDiv)}, false);
+registerButton.addEventListener("click", function() {giveFocusToDiv(registerDiv)}, false);
+welcome_register_button.addEventListener("click", function() {giveFocusToDiv(registerDiv)}, false);
+
+
+//Login page 
 const loginDiv = document.getElementById("loginDiv");
 var loginUsername = document.getElementById("loginUsername");
 var loginPassword = document.getElementById("loginPassword");
@@ -16,74 +23,6 @@ const loginSubmit = document.getElementById("loginSubmit");
 const login_pop_up = document.getElementById("login_pop_up");
 const login_pop_up_continue_button = document.getElementById("login_pop_up_continue_button");
 
-
-//Register page attributes
-const registerDiv = document.getElementById("registerDiv");
-var registerUsername = document.getElementById("registerUsername");
-var registerPassword = document.getElementById("registerPassword");
-var registerRepeatPassword = document.getElementById("repeat-password");
-const registerSubmit = document.getElementById("registerSubmit");
-
-//About
-const aboutButton = document.getElementById('aboutButton');
-const about_us_popup = document.getElementById('aboutDiv');
-const closeButton = document.getElementById('closeButton');
-
-window.addEventListener("click", function(event) {
-  // Close the pop-up dialog when clicking outside of it
-  if (about_us_popup.style.display === 'block' && event.target !== aboutDiv && event.target !== aboutButton && !about_us_popup.contains(event.target)){
-    about_us_popup.style.display = "none";  
-  }
-  });
-
-// Show the popup when the "About us" button is clicked
-aboutButton.addEventListener('click', function() {
-  about_us_popup.style.display = 'block';
-});
-
-// Hide the popup when the close button is clicked
-closeButton.addEventListener('click', function() {
-  about_us_popup.style.display = 'none';
-});
-
-
-
-window.addEventListener("keydown", function(event) {
-  // Close the pop-up dialog when pressing the Escape key
-  if (event.key === "Escape") {
-    about_us_popup.style.display = "none";
-  }
-});
-
-//Divs
-var appDivs = [logoDiv, loginDiv, registerDiv, configDiv, gameDiv, welcome_login_button, welcome_register_button, footer, endGameDiv, login_pop_up] //ADD MORE DIVS WHEN THEY ARE ADDED TO THE APP!!
-
-//Users dictionary
-var users = {"s" : "s"};
-
-function giveFocusToDiv(divToFocus) {
-  for(let i=0; i<appDivs.length; i++) {
-    appDivs[i].style.display = "none"
-  }
-  if (divToFocus === logoDiv){
-    welcome_register_button.style.display="block"
-    welcome_login_button.style.display="block"
-  }
-  if (divToFocus != gameDiv){
-    footer.style.display="block"
-  }
-  
-  divToFocus.style.display="block"
-}
-
-//Main page methods
-homeButton.addEventListener("click", function() {giveFocusToDiv(logoDiv)}, false);
-loginButton.addEventListener("click", function() {changeLoginText(); giveFocusToDiv(loginDiv)}, false);
-welcome_login_button.addEventListener("click", function() {giveFocusToDiv(loginDiv)}, false);
-registerButton.addEventListener("click", function() {giveFocusToDiv(registerDiv)}, false);
-welcome_register_button.addEventListener("click", function() {giveFocusToDiv(registerDiv)}, false);
-
-//Login page methods
 loginSubmit.addEventListener("click", function() {loginUser(loginUsername.value, loginPassword.value)})
 login_pop_up_continue_button.addEventListener("click", function() {giveFocusToDiv(configDiv)})
 
@@ -97,7 +36,7 @@ function loginUser(username, password) {
   if (users[username] && users[username] === password) {
     console.log("Login successful.");
     var config_h1 = document.querySelector('#configDiv h1'); // Select the h1 element
-    config_h1.textContent = 'Welcome ' + loginUsername.value + "! Please configure the game settings ans start playing.";
+    config_h1.textContent = 'Welcome ' + username + "! Please configure the game settings and start playing.";
     loginButton.textContent = "Log Out"
     giveFocusToDiv(login_pop_up);
   } else {
@@ -108,8 +47,18 @@ function loginUser(username, password) {
   }
 }
 
-//Register page methods
+
+//Register page
+const registerDiv = document.getElementById("registerDiv");
+var registerUsername = document.getElementById("registerUsername");
+var registerPassword = document.getElementById("registerPassword");
+var registerRepeatPassword = document.getElementById("repeat-password");
+const registerSubmit = document.getElementById("registerSubmit");
+const register_pop_up = document.getElementById("register_pop_up");
+const register_pop_up_continue_button = document.getElementById("register_pop_up_continue_button");
+
 registerSubmit.addEventListener("click", function() {registerUser(registerUsername.value, registerPassword.value, registerRepeatPassword.value)})
+register_pop_up_continue_button.addEventListener("click", function() {giveFocusToDiv(loginDiv)})
 
 function registerUser(username, password, repeat) {
   console.log("hi hi")
@@ -128,13 +77,72 @@ function registerUser(username, password, repeat) {
   else{
     users[username] = password
     console.log("User registered successfully.");
-    alert("Hi " + username + "! Welcome to space invaders game. Please login to start playing.");
-    for (const key of Object.keys(users)) {
-      console.log(key + ":" + users[key])
-    }
-    giveFocusToDiv(loginDiv)
+    var pop_up_h1 = document.querySelector('#register_pop_up h1'); // Select the h1 element
+    pop_up_h1.textContent = 'Welcome ' + username + "to Guardians of the Galaxy game! Please login and start playing.";
+    giveFocusToDiv(register_pop_up)
   }
 }
+
+
+//About
+const aboutButton = document.getElementById('aboutButton');
+const about_us_popup = document.getElementById('aboutDiv');
+const closeButton = document.getElementById('closeButton');
+
+window.addEventListener("click", function(event) {
+  // Close the pop-up dialog when clicking outside of it
+  if (about_us_popup.style.display === 'block' && event.target !== aboutDiv && event.target !== aboutButton && !about_us_popup.contains(event.target)){
+    about_us_popup.style.display = "none";  
+  }
+});
+
+// Show the popup when the "About us" button is clicked
+aboutButton.addEventListener('click', function() {
+  about_us_popup.style.display = 'block';
+});
+
+// Hide the popup when the close button is clicked
+closeButton.addEventListener('click', function() {
+  about_us_popup.style.display = 'none';
+});
+
+window.addEventListener("keydown", function(event) {
+  // Close the pop-up dialog when pressing the Escape key
+  if (event.key === "Escape") {
+    about_us_popup.style.display = "none";
+  }
+});
+
+
+//Divs
+var appDivs = [logoDiv, loginDiv, registerDiv, configDiv, gameDiv, welcome_login_button, welcome_register_button, footer, endGameDiv, login_pop_up, register_pop_up] //ADD MORE DIVS WHEN THEY ARE ADDED TO THE APP!!
+const end_game_pop_up = document.getElementById('end_game_pop_up');
+const end_game_pop_up_continue_button = document.getElementById('end_game_pop_up_continue_button');
+const end_game_pop_up_cancel_button = document.getElementById('end_game_pop_up_cancel_button');
+
+function giveFocusToDiv(divToFocus) {
+  for(let i=0; i<appDivs.length; i++) {
+    appDivs[i].style.display = "none"
+  }
+  if (divToFocus === logoDiv){
+    welcome_register_button.style.display="block"
+    welcome_login_button.style.display="block"
+  }
+  if (divToFocus != gameDiv){
+    footer.style.display="block"
+  }
+  
+  divToFocus.style.display="block"
+}
+
+function isGameOn(){
+  return gameDiv.style.display === 'block'
+}
+
+
+//Users dictionary
+var users = {"s" : "s"};
+
 
 
 function sleep(milliseconds) {
