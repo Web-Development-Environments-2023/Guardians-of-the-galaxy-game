d8940c94007a1bfa5dc569e53e5dbd12bfb4c9eb
LiorMish <liormishutin24@gmail.com>
2023-04-21 12:09:25 +0300

    edit css

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const configForm = document.getElementById('configForm');
 const startGameButton = document.getElementById("startGameButton");
+const scoresButton = document.getElementById("scoresButton");
 var fireButtonInput;
 const gameDiv = document.getElementById("gameDiv");
 var timeLimit;
@@ -20,3 +21,5 @@ configForm.addEventListener("submit", function(event) {
 
     // newGame();
 });
+
+scoresButton.addEventListener("click", function() {giveFocusToDiv(endGameDiv)})
